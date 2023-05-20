84d38ed143533a9e3faadc6cf2ae4f1050aaeee4
NeonTech707 <mohammad.su3@outlook.com>
2023-05-20 07:07:13 +0100

    Improved about section

---
+++ src/home.js
@@ -0,0 +1,15 @@
+/* TO-DO
+- Add question mark with profile image
+- Finish projects section
+- Finish contact section
+- Finish footer
+- Add patterns on sides of section headings
+- Change scrollbar css
+- Change colour of nav depedning on the section the user is viewing (with fade effect)
+- Add animations (Scrolling skills, moving dash for profile, typing/slide in animations for home sections/other text?)
+- Add interactive background (triangular moving waves top and bottom, faint binary background with interactivity) for home sections
+- Add non-interactive background for other sections (floating and moving binary)
+- Make scroll lock to section/change scroll speed
+- Make responsive and suitable for mobile
+- Make page colour changeable? (CSS hue filter, ignore project images, add this feature on the top of page at home section which appears slowly with animation, when user scrolls, it will look like it connects with the navbar)
+*/
