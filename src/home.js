78effaaf8d49a956b398b8c6730857ce6b86969c
NeonTech707 <mohammad.su3@outlook.com>
2023-05-25 08:31:26 +0100

    Customised cursor and scrollbar

--- src/home.js
+++ src/home.js
@@ -2,16 +2,19 @@
 - Make project images clickable
 - Orange grid or cross-hatch for project image background?
 - Make project buttons dashed border?
-- Finish projects section
+- Add dashed border around project info?
+- Fix "a" element css
+- Add design to "a" project elements
+- Add green hue and blur filter to navbar?
 - Finish contact section
 - Finish footer
 - Add patterns on sides of section headings
-- Change scrollbar css
-- Change colour of nav depedning on the section the user is viewing (with fade effect)
+- Change colour of nav depedning on the section the user is viewing (with fade animation)
 - Add animations (Scrolling skills, moving dash for profile, typing/slide in animations for home sections/other text?)
 - Add interactive background (triangular moving waves top and bottom, faint binary background with interactivity) for home sections
 - Add non-interactive background for other sections (floating and moving binary)
 - Make scroll lock to section/change scroll speed
 - Make responsive and suitable for mobile
 - Make page colour changeable? (CSS hue filter, ignore project images, add this feature on the top of page at home section which appears slowly with animation, when user scrolls, it will look like it connects with the navbar)
+- Change demo links to google drive/dropbox
 */
