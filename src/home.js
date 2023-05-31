c085eaffdf9a6968f898734abe8c97d056002b87
NeonTech707 <mohammad.su3@outlook.com>
2023-05-31 12:26:48 +0100

    Edited styling in project section

--- src/home.js
+++ src/home.js
@@ -1,20 +1,18 @@
 /* TO-DO
 - Make project images clickable
-- Orange grid or cross-hatch for project image background?
-- Make project buttons dashed border?
-- Add dashed border around project info?
-- Fix "a" element css
-- Add design to "a" project elements
-- Add green hue and blur filter to navbar?
+- Change colour of navbar links depedning on the section the user is viewing (with fade animation)
 - Finish contact section
 - Finish footer
+- Add "view my work" or "scroll down", etc. on home section
+- Add green hue and blur filter to navbar?
 - Add patterns on sides of section headings
-- Change colour of nav depedning on the section the user is viewing (with fade animation)
 - Add animations (Scrolling skills, moving dash for profile, typing/slide in animations for home sections/other text?)
 - Add interactive background (triangular moving waves top and bottom, faint binary background with interactivity) for home sections
 - Add non-interactive background for other sections (floating and moving binary)
 - Make scroll lock to section/change scroll speed
 - Make responsive and suitable for mobile
+- Check if website looks good on other browsers (firefox, bing, opera, IE, etc.)
 - Make page colour changeable? (CSS hue filter, ignore project images, add this feature on the top of page at home section which appears slowly with animation, when user scrolls, it will look like it connects with the navbar)
+- Change dash styles to be longer?
 - Change demo links to google drive/dropbox
 */
