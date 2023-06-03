79f90236a2a7afd9fe1acec9f11289cb4dc583e6
NeonTech707 <mohammad.su3@outlook.com>
2023-06-03 11:08:52 +0100

    Added to contact page

--- src/home.js
+++ src/home.js
@@ -3,9 +3,10 @@
 - Change colour of navbar links depedning on the section the user is viewing (with fade animation)
 - Finish contact section
 - Finish footer
+- Add favicon
 - Add "view my work" or "scroll down", etc. on home section
 - Add green hue and blur filter to navbar?
-- Add patterns on sides of section headings
+- Add patterns on sides of section headings (Scale >> Text << Animation with letters of heading slowly changing to a special character/number e.g "A" changing to 4, "o" changing to "0", etc. )
 - Add animations (Scrolling skills, moving dash for profile, typing/slide in animations for home sections/other text?)
 - Add interactive background (triangular moving waves top and bottom connected with binary digits, faint binary background with interactivity or binary digits that float and bounce as particles and connect with lines with the cursor) for home section
 - Add non-interactive background for other sections (floating and moving binary)
@@ -15,4 +16,6 @@
 - Make page colour changeable? (Sass variable - change lime to $primary-color, manually add colour filters for images, add this feature on the top of page at home section which appears slowly with animation, when user scrolls, it will look like it connects with the navbar)
 - Change dash styles to be longer?
 - Change demo links to google drive/dropbox, embed videos?
+- "Show more" button after adding another project
+- SQL, PHP project (full-stack project?)
 */
