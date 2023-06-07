b3ccb4bead6efa625b5af47ef6451c1b9f9f2659
NeonTech707 <mohammad.su3@outlook.com>
2023-06-07 11:57:57 +0100

    Added jump links

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,14 +1,16 @@
 /* TO-DO
 - Make project images clickable
+- Make GitHub logo clickable and add back to top button
 - Change colour of navbar links depedning on the section the user is viewing (with fade animation), add anchor/click function
 - Finish submit button and error
+- Add mail background image to contact section
 - Netlify custom success page
 - Finish footer
 - Add favicon
 - Add "view my work" or "scroll down", etc. on home section
 - Add green hue and blur filter to navbar?
-- Add patterns on sides of section headings (Scale >> Text << Animation with letters of heading slowly changing to a special character/number e.g "A" changing to 4, "o" changing to "0", etc. )
-- Add hover animations on buttons/anchors
+- Add patterns on sides of section headings (Scale >> Text << Animation with letters of heading slowly changing to a special character/number e.g "A" changing to 4, "o" changing to "0", etc. glow effect )
+- Add hover animations on buttons/anchors with glow effect
 - Add animations (Scrolling skills, moving dash for profile, typing/slide in animations for home sections/other text?)
 - Add interactive background (triangular moving waves top and bottom connected with binary digits, faint binary background with interactivity or binary digits that float and bounce as particles and connect with lines with the cursor) for home section
 - Add non-interactive background for other sections (floating and moving binary)
@@ -21,3 +23,12 @@
 - "Show more" button after adding another project
 - SQL, PHP project (full-stack project?)
 */
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > $('.home-overlay').height()) {
+        $('#home-cont').hide();
+    }
+    else {
+        $('#home-cont').show();
+    }
+});
