7d4e0db4b1f451b286c1e238556bc8d841cec6dc
NeonTech707 <mohammad.su3@outlook.com>
2023-06-09 10:00:11 +0100

    Added link/button animations on hover

--- src/components/nav-bar/nav-bar.js
+++ src/components/nav-bar/nav-bar.js
@@ -1,15 +1,3 @@
-let options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5,
-}
-
-let options2 = { // For projects section as it is longer
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.2,
-}
-
 const navItems = $('.nav-item');
   
 function navActive(entries, observer) { // Change highlighted nav-item depending on page position
@@ -21,10 +9,22 @@ function navActive(entries, observer) { // Change highlighted nav-item depending
     })
 }
 
+let options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5,
+}
+
+let options_2 = { // For projects section as it is longer
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2,
+}
+
 let observerNav = new IntersectionObserver(navActive, options);
 observerNav.observe($('#home')[0]);
 observerNav.observe($('#about')[0]);
 observerNav.observe($('#contact')[0]);
 
-let observerNavProjects = new IntersectionObserver(navActive, options2);
+let observerNavProjects = new IntersectionObserver(navActive, options_2);
 observerNavProjects.observe($('#projects')[0]);
