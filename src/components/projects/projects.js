ad6820593b65e3567a0f1629a07f2b023408e77a
NeonTech707 <114197412+Mohammad-SU@users.noreply.github.com>
2024-10-11 05:10:47 +0100

    added slider for limedrive.

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,55 +1,98 @@
-var projectSlideAnimation_timeout
-let translateNum = rem(-48.6875)
+function initializeSlider(containerId) {
+    const container = $(`#${containerId}`);
+    const imageContainer = container.find('.project-image-cont');
+    const buttons = container.find('.slide-button');
+    const slideSize = rem(48.6875);
+    let translateNum = 0;
+    let slideCount = buttons.length;
+    let animationTimeout;
+    let slideAutoDelay = 3000 // For when no slide button is clicked, or after a click after the manual delay is finished
+    let slideManualDelay = 8000 // For when a slide button is clicked (so the slide stays longer so the user can study it better)
 
-function setAnimation(functionName, time) {
-    projectSlideAnimation_timeout = setTimeout(functionName, time)
+    function nextSlide() {
+        imageContainer.css({transition: "transform 700ms", transform: `translate(${translateNum}px)`});
+        translateNum -= slideSize; // Decrement so that it translates the images left (to move the slider 'right')
+    }
+
+    function slideAnimation() {
+        // After the last slide, a duplicate of the first slide exists to make it seem like the slider is endless.
+        // After the duplicate is fully in view, then the slider position should reset to the first instantly without animation
+        const duplicateSlideReached = translateNum === -slideSize * (slideCount + 1)
+
+        if (duplicateSlideReached) {
+            imageContainer.css({transition: "none", transform: "translate(0px)"});
+            translateNum = 0;
+
+            // Buffer
+            setTimeout(() => {
+                nextSlide()
+            }, 25);
+        } 
+        else {
+            nextSlide()
+        }
+        animationTimeout = setTimeout(() => slideAnimation(), slideAutoDelay);
+    }
+
+    buttons.on("click", function() {
+        const buttonIndex = parseInt($(this).text()) - 1; // -1 as index is +1 higher
+        translateNum = -slideSize * buttonIndex;
+        imageContainer.css({transition: "transform 700ms", transform: `translate(${translateNum}px)`});
+        clearTimeout(animationTimeout);
+        animationTimeout = setTimeout(() => slideAnimation(), slideManualDelay);
+    });
+
+    return {
+        start: () => {
+            animationTimeout = setTimeout(() => slideAnimation(), slideAutoDelay);
+        },
+        stop: () => {
+            clearTimeout(animationTimeout);
+        },
+        reset: () => {
+            clearTimeout(animationTimeout);
+            translateNum = 0;
+            imageContainer.css({transition: "transform 700ms", transform: "translate(0px)"});
+        }
+    };
 }
 
-function projectSlideAnimation () {
-    if (translateNum == rem(-243.4375)) { // if img 5 is reached (duplicate of first img)
-        $(".project-image-cont").css({transition: "none", transform: "translate(0px)"})
-        translateNum = rem(-48.6875)
-        setAnimation(projectSlideAnimation, 25)
-    }
-    else if (translateNum > rem(-243.4375)) {
-        $(".project-image-cont").css({transition: "transform 700ms", transform: "translate("+translateNum+"px)"})
-        translateNum -= rem(48.6875)
-        setAnimation(projectSlideAnimation, 3000)
-    }
-}
-
-$(".slide-button").on("click", function() {
-    translateNum = parseInt($(this).text()) * rem(-48.6875) + rem(48.6875) // Addition due to duplicate of first img
-    $(".project-image-cont").css({transition: "transform 700ms", transform: "translate("+translateNum+"px)"})
-    clearTimeout(projectSlideAnimation_timeout)
-    setAnimation(projectSlideAnimation, 8000)
-    translateNum -= rem(48.6875)
-})
+let limedriveSlider, notesSlider;
 
 function projectsFadeIn() {
-        if (viewportVis($("#project-1")) && !$("#project-1 .slider-cont").hasClass("projects__fade-in")) {
-            $("#project-1 .slider-cont, #project-1 .project-info").addClass("projects__fade-in")
-            setAnimation(projectSlideAnimation, 3000)
+        if (viewportVis($("#limedrive-project")) && !$("#limedrive-project .slider-cont").hasClass("projects__fade-in")) {
+            $("#limedrive-project .slider-cont, #limedrive-project .project-info").addClass("projects__fade-in");
+            limedriveSlider = initializeSlider("limedrive-project");
+            limedriveSlider.start();
         } 
-        if (viewportVis($("#project-2")) && !$("#project-2 .project-info").hasClass("projects__fade-in")) {
-            $("#project-2 .project-image, #project-2 .project-info").addClass("projects__fade-in")
-        }        
-        if (viewportVis($("#project-3")) && !$("#project-3 .project-info").hasClass("projects__fade-in")) {
-            $("#project-3 .project-image, #project-3 .project-info").addClass("projects__fade-in")
+
+        if (viewportVis($("#notes-project")) && !$("#notes-project .slider-cont").hasClass("projects__fade-in")) {
+            $("#notes-project .slider-cont, #notes-project .project-info").addClass("projects__fade-in");
+            notesSlider = initializeSlider("notes-project");
+            notesSlider.start();
+        } 
+
+        if (viewportVis($("#connect-project")) && !$("#connect-project .project-info").hasClass("projects__fade-in")) {
+            $("#connect-project .project-image, #connect-project .project-info").addClass("projects__fade-in")
+        }  
+
+        if (viewportVis($("#calculator-project")) && !$("#calculator-project .project-info").hasClass("projects__fade-in")) {
+            $("#calculator-project .project-image, #calculator-project .project-info").addClass("projects__fade-in")
         }
-        if (viewportVis($("#project-4")) && !$("#project-4 .project-info").hasClass("projects__fade-in")) {
-            $("#project-4 .project-image, #project-4 .project-info").addClass("projects__fade-in")
-        }    
 }
+
 $(window).scroll(projectsFadeIn)
 projectsFadeIn()
 
 function windowResizeProjects() {
-    clearTimeout(projectSlideAnimation_timeout)
-    translateNum = 0
-    $(".project-image-cont").css({transition: "transform 700ms", transform: "translate("+translateNum+"px)"})
-    if ($("#project-1 .slider-cont").hasClass("projects__fade-in")) {
-        setAnimation(projectSlideAnimation, 3000)
+    if (limedriveSlider) limedriveSlider.reset();
+    if (notesSlider) notesSlider.reset();
+    
+    if ($("#limedrive-project .slider-cont").hasClass("projects__fade-in")) {
+        limedriveSlider.start();
+    }
+    if ($("#notes-project .slider-cont").hasClass("projects__fade-in")) {
+        notesSlider.start();
     }
 
     if ($(window).width() <= 1332 && $(window).width() >= 1301) {
@@ -68,16 +111,19 @@ function windowResizeProjects() {
         $(".project-live-game").text("LIVE GAME")
     }
 
-    let projectRightImageAnchors = document.querySelectorAll(".project-right__image-anchor")
-    let projectRightInfos = document.querySelectorAll(".project-right .project-info")
+    let connectProjectRightAnchor = document.querySelector("#connect-project .project-right__image-anchor")
+    let connectProjectRightInfo = document.querySelector("#connect-project .project-info")
+
+    let limedriveProjectRightSlider = document.querySelector("#limedrive-project .slider-cont")
+    let limedriveProjectRightInfo = document.querySelector("#limedrive-project .project-info")
 
     if ($(window).width() <= 1300) {
-        projectRightImageAnchors[0].after(projectRightInfos[0])
-        projectRightImageAnchors[1].after(projectRightInfos[1])
+        connectProjectRightAnchor.after(connectProjectRightInfo)
+        limedriveProjectRightSlider.after(limedriveProjectRightInfo)
     }
     if ($(window).width() > 1300) {
-        projectRightImageAnchors[0].before(projectRightInfos[0])
-        projectRightImageAnchors[1].before(projectRightInfos[1])
+        connectProjectRightAnchor.before(connectProjectRightInfo)
+        limedriveProjectRightSlider.before(limedriveProjectRightInfo)
     }
 }
 windowResizeProjects()
